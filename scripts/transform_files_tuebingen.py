1d177c4bfd91aaf7bb08ff52a42d6443ab874df3
jgciudad <jgciudad@outlook.com>
2024-01-03 22:33:41 +0100

    adapted to load kornum lab data

--- scripts/transform_files_tuebingen.py
+++ scripts/transform_files_tuebingen.py
@@ -9,6 +9,9 @@ from os.path import join, basename, isfile, realpath, dirname
 import h5py
 import numpy as np
 import tables
+import pandas as pd
+import mne
+import scipy
 
 sys.path.insert(0, realpath(join(dirname(__file__), '..')))
 
@@ -23,6 +26,81 @@ def parse():
                         help='name of experiment to transform data to')
 
     return parser.parse_args()
+
+def load_kornum_labels(label_path):
+    # files = [fn for fn in glob(join(config.DATA_DIR, '**/*.tsv'), recursive=True)]
+    # labels_path = [f for f in files if rec_name in f]
+    # if len(labels_path) == 1:
+    #     labels_path = labels_path[0]
+    # else:
+    #     raise Exception("More than one file with recording name " + rec_name)
+    df = pd.read_csv(join(config.DATA_DIR, label_path), skiprows=9, engine='python', sep='\t', index_col=False).iloc[:, 4]
+    # df.loc[(df != 1) & (df != 2) & (df != 3)] = 4
+
+    # transform integer labels to category name
+    def int_class_to_string(row):
+        if row == 1:
+            return 'WAKE'
+        if row == 2:
+            return 'NREM'
+        if row == 3:
+            return 'REM'
+        if (row != 1) & (row != 2) & (row != 3):
+            return 'ARTIFACT' 
+    stages = df.apply(int_class_to_string)
+
+    return stages.to_list(), df.index.values.tolist()
+
+def load_raw_recording(label_path):
+    """
+    :param file_path: path to the .edf recording
+    :param resample_rate: new sampling rate in Hertz
+    :return: numpy array with the signal
+    """
+    signal_path = join(config.DATA_DIR, label_path.split("/")[0], 'EDF', basename(label_path)[:-4] + '.edf')
+    
+    data = mne.io.read_raw_edf(signal_path)
+    raw_data = data.get_data()
+    info = data.info
+    channels = data.ch_names
+
+    features = {}
+
+    for c, channel in enumerate(channels):
+        # if resample_rate:
+        # new_num_samples = raw_data.shape[1]/info['sfreq']*resample_rate
+        # if new_num_samples.is_integer() is False:
+        #     raise Exception("New number of samples is not integer")
+                    # raw_data = scipy.signal.resample(x=raw_data, num=int(new_num_samples), axis=1)
+        
+        if channel == 'EEG EEG1A-B':
+            c_name = 'EEG1'
+        elif channel == 'EEG EEG2A-B':
+            c_name = 'EEG2'
+        elif channel == 'EMG EMG':
+            c_name = 'EMG'
+
+        features[c_name] = downsample(raw_data[c, :], sr_old=info['sfreq'], sr_new=config.SAMPLING_RATE,
+                                            fmax=0.4 * config.SAMPLING_RATE, outtype='sos', method='pad')
+
+    sample_start_times = np.arange(0, features[c_name].shape[0], config.SAMPLING_RATE*config.SAMPLE_DURATION, dtype=int)
+
+    return features, sample_start_times.tolist()
+
+def read_kornum_recording(label_path: str, dataset: str):
+    """reads data from rec_name.edf and rec_name.tsv
+
+    Returns:
+        (dict, list, list): features in the form of a dict with entries for each CHANNEL, labels as a list, list of
+        start times of samples as indexes in features
+    """
+
+    labels, epoch_number = load_kornum_labels(label_path)
+    features_dict, sample_start_times = load_raw_recording(label_path)
+
+    return features_dict, labels, sample_start_times, epoch_number
+
+
 
 
 def read_h5(h5_f_name: str):
@@ -65,7 +143,7 @@ def read_h5(h5_f_name: str):
     return features, labels, sample_start_times
 
 
-def write_data_to_table(table: tables.Table, features: dict, labels: list, start_times: list, mouse_id: int):
+def write_data_to_table(table: tables.Table, features: dict, labels: list, start_times: list, mouse_id: int, epoch_number: list):
     """writes given data to the passed table, each sample is written in a new row"""
     sample = table.row
 
@@ -98,7 +176,7 @@ def write_data_to_table(table: tables.Table, features: dict, labels: list, start
 def get_files_for_dataset(dataset: str):
     """determines which files to transform for a given dataset, see DATA_SPLIT"""
     # load all h5 files in DATA_DIR and sort them
-    files = [basename(fn) for fn in glob(join(config.DATA_DIR, '*.h5'))]
+    files = [basename(fn) for fn in glob(join(config.DATA_DIR, '**/*.tsv'), recursive=True)]
     files.sort()
 
     # search for files corresponding to mice name in DATA_SPLIT in files
@@ -113,16 +191,16 @@ def transform():
     table_desc = create_table_description(config)
 
     # if the transformed data file already exists, ask the user if he wants to overwrite it
-    if isfile(config.DATA_FILE):
-        question = f"{realpath(config.DATA_FILE)} already exists, do you want to override? (y/N): "
-        response = input(question)
-        if response.lower() != 'y':
-            exit()
+    # if isfile(config.DATA_FILE):
+    #     question = f"{realpath(config.DATA_FILE)} already exists, do you want to override? (y/N): "
+    #     response = input(question)
+    #     if response.lower() != 'y':
+    #         exit()
 
     print(f'data is loaded from {realpath(config.DATA_DIR)}')
 
     # open pytables DATA_FILE
-    with tables.open_file(config.DATA_FILE, mode='w', title='data from Tuebingen') as f:
+    with tables.open_file(config.DATA_FILE, mode='w', title='data from Kornum') as f:
 
         # datasets to create, if there is a dataset without any data, an empty table is created inside DATA_FILE
         datasets = ['train', 'valid', 'test']
@@ -134,18 +212,18 @@ def transform():
             table = f.create_table(f.root, dataset, table_desc, dataset + ' data')
 
             # determine which files to transform for each dataset based on DATA_SPLIT
-            file_list = get_files_for_dataset(dataset)
+            # file_list = get_files_for_dataset(dataset)
             # iterate over files, load them and write them to the created table
-            for i, f_name in enumerate(file_list):
-                print('mouse [{:d}/{:d}]: {:s}'.format(i + 1, len(file_list), f_name))
+            for i, label_path in enumerate(config.DATA_SPLIT[dataset]):
+                print('mouse [{:d}/{:d}]: {:s}'.format(i + 1, len(config.DATA_SPLIT[dataset]), label_path))
                 start = time.time()
 
                 # load only numbers in mice name
-                mouse_id = int(''.join(filter(str.isdigit, f_name)))
+                # mouse_id = int(''.join(filter(str.isdigit, f_name)))
                 # read h5 file, load and downsample data
-                features, labels, times = read_h5(f_name)
+                features, labels, times, epoch_number = read_kornum_recording(label_path, dataset)
                 # write loaded data to table
-                write_data_to_table(table, features, labels, times, mouse_id)
+                write_data_to_table(table, features, labels, times, label_path, epoch_number)
 
                 print('execution time: {:.2f}'.format(time.time() - start))
                 print()
@@ -156,7 +234,7 @@ def transform():
 if __name__ == '__main__':
     args = parse()
     # load config, dirs are not needed here, because we do not write a log
-    config = ConfigLoader(args.experiment, create_dirs=False)
+    config = ConfigLoader(experiment='kornum_config', create_dirs=False)
 
     # transform files
     transform()
